232bb8fc59e05b832ae5b4e4e80538024b847a26
Nidhi Sharma <155046978+Nidhi-Sharma04@users.noreply.github.com>
2025-03-13 07:03:23 +0000

    const var let

---
+++ basics/01variables.js
@@ -0,0 +1,10 @@
+//constant
+const accountId = 14453
+let accountEmail="nidhi@google.com"
+var account ="12345"
+accountCity="jaipur"
+let accounthlo;
+// accountId=90
+console.log(accountId)
+console.table([accountId,accountEmail,account,accountCity,accounthlo])
+

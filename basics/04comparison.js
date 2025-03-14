c1e6af9114e9ce64cda0a30a966e2a6c91c076f6
Nidhi Sharma <155046978+Nidhi-Sharma04@users.noreply.github.com>
2025-03-14 09:38:40 +0000

    comparison

---
+++ basics/04comparison.js
@@ -0,0 +1,17 @@
+console.log(2>1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+console.log("2">1)
+console.log("02">1)
+//here < > >= <= will treat null as 0 but undefined will not be treated as 0 it wil be treated as undefined only
+//but ==will not do so it will not do any conversion for null 
+//so null==null or null==undefined ..will give true else false
+console.log(null>=0)
+console.log(null==null)
+console.log(null<0)
+// === will do strict checking it will not do any type conversion
+console.log("2"===1)//false 
+
+
+

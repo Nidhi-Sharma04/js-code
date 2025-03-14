d96b385b21419294ad934c8666de8065aae34499
Nidhi Sharma <155046978+Nidhi-Sharma04@users.noreply.github.com>
2025-03-14 07:45:21 +0000

    conversions

--- basics/03conversionOperation.js
+++ basics/03conversionOperation.js
@@ -2,13 +2,13 @@
 // let score=undefined/
 // let score="nidhi"
 // let score=true
-let score=false
-console.log(score)
-console.log(typeof(score))
-let coverted=Number(score)
-console.log(typeof(score))
-console.log(typeof(coverted))
-console.log(coverted)//nan not a number
+// let score=false
+// console.log(score)
+// console.log(typeof(score))
+// let coverted=Number(score)
+// console.log(typeof(score))
+// console.log(typeof(coverted))
+// console.log(coverted)//nan not a number
 //conversion to numbers 
 /** 
  "33"--> 33
@@ -16,13 +16,13 @@ console.log(coverted)//nan not a number
  true-->1
  false-->0
  */
-console.log("***************************")
+// console.log("***************************")
 // let isloggedin=1
 // let isloggedin=""
-let isloggedin="nidhi"
-let nos=Boolean(isloggedin)
-console.log(nos)
-console.log(typeof(nos))
+// let isloggedin="nidhi"
+// let nos=Boolean(isloggedin)
+// console.log(nos)
+// console.log(typeof(nos))
 //conversion to boolean
 /**
  * 1-->true
@@ -31,7 +31,49 @@ console.log(typeof(nos))
  * "nidhi"-->true
  */
 //cnversion to string
-let nos1=33
-let some=String(nos1)
-console.log(nos1)
-console.log(typeof(some))
+// let nos1=33
+// let some=String(nos1)
+// console.log(nos1)
+// console.log(typeof(some))
+console.log("*******************conversions****************************")
+// let value=3
+// let neg=-(value)
+// console.log(neg)
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%2);
+// console.log(2**2);
+let str1="nidhi"
+let str2=" sharma"
+let str3=str1+str2
+console.log(str3)
+console.log("1"+2)//12
+console.log(1+"2")//12
+console.log("1"+2+3)//123
+console.log(1+2+"3")//33
+console.log(1+"2"+3+4)//1234
+console.log("3"+3+4+6+7)//33467
+console.log("3"*4)//12
+console.log(4*"3")//12
+console.log("9"*"9")//81
+console.log("hlo"*3)//nan
+console.log("hlo"*"good")//nan
+console.log(3+4*5%3)//here * and% has same precedence but + has lower precedence so will evaluate from left to right
+//4*5=20
+//20%3=2
+//3+2=5
+console.log(true)//true
+console.log(+true)//1
+//console.log(true+)//will give error
+console.log(-true)//-1
+console.log(false)//false
+console.log(+"")//0
+//by writing+ bolean gets converted to number
+let num1,num2,num3
+num1=num2=num3=2+2
+console.log(num2)
+let counter=100
+counter=++counter+counter+counter
+console.log(counter)

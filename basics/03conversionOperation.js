8a661690d8fc0d3412bb5c7409cc1c9ed3a1e936
Nidhi Sharma <155046978+Nidhi-Sharma04@users.noreply.github.com>
2025-03-14 06:55:11 +0000

    conversions

---
+++ basics/03conversionOperation.js
@@ -0,0 +1,37 @@
+// let score="33abc";
+// let score=undefined/
+// let score="nidhi"
+// let score=true
+let score=false
+console.log(score)
+console.log(typeof(score))
+let coverted=Number(score)
+console.log(typeof(score))
+console.log(typeof(coverted))
+console.log(coverted)//nan not a number
+//conversion to numbers 
+/** 
+ "33"--> 33
+ "33abc"--> nan
+ true-->1
+ false-->0
+ */
+console.log("***************************")
+// let isloggedin=1
+// let isloggedin=""
+let isloggedin="nidhi"
+let nos=Boolean(isloggedin)
+console.log(nos)
+console.log(typeof(nos))
+//conversion to boolean
+/**
+ * 1-->true
+ * 0-->false
+ * ""-->false
+ * "nidhi"-->true
+ */
+//cnversion to string
+let nos1=33
+let some=String(nos1)
+console.log(nos1)
+console.log(typeof(some))

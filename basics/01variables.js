e27cbd2cd8cea12a449d1d100cef369b13ecda78
Nidhi Sharma <155046978+Nidhi-Sharma04@users.noreply.github.com>
2025-03-14 06:29:42 +0000

    DATATYPES

--- basics/01variables.js
+++ basics/01variables.js
@@ -3,7 +3,7 @@ const accountId = 14453
 let accountEmail="nidhi@google.com"
 var account ="12345"
 accountCity="jaipur"
-let accounthlo;
+let accounthlo
 // accountId=90
 console.log(accountId)
 console.table([accountId,accountEmail,account,accountCity,accounthlo])
---
+++ basics/02data_type.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3)--we are using nodejs and not browser
+// console.log(3+3) console.log("hlo")--will give error as these two statements must be separated
+console.log(3+3); console.log("hlo")//this is correct but coder must take care of readability
+//javascript automatically avoid spaces;
+//ecma---decide all the standards for javacscript
+
+/*DATA TYPES */
+let name="nidhi" // automatically string
+let age=18 //number
+let isloggedin=true //boolean
+//number--range 2^53
+//bigint--when number is very large than number range
+//string --'' " "
+//boolean --true/false
+//null --stanalone value and datatype
+//undefined--value is not assigned
+//eg:
+let val;//undefined *****************IQ
+let vale=null;//null*******************IQ
+//symbol--used in react when we need to find uniqueness of components. also used by figma tool
+//object--
+
+console.log(typeof age)
+console.log(typeof null)//type is object****************************IQ
+console.log(typeof undefined)//type is undefined*************************IQ
